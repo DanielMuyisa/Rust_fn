77591867686b59aecba6fead5763cd1451b2c716
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-11 20:37:03 +0100

    handle class

--- fn.rs
+++ fn.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello world");
+    // let mut age = 13;
+    // println!("{}", age);
+    // age = 32;
+    // println!("{}", age);
+
+    // # vecteur 
+    // let date = vec!(2021, 2022);
+    // print(&date);
+    // print(&date);
+
+
+    // 3
+
+}
+
+//  Vecteur
+// fn print(dn: &Vec<i32>) {
+//     println!("date is {:?}", dn);
+// }
+
+
+// regrouper des champs
+    // structure = pas de comportement
+    // class = avec comportent
+struct Foo {
+    value: i32
 }
