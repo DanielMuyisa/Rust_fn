93d85187ee98b2ef7a16c9a031ac364d7dfd0580
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-11 21:24:26 +0100

    vecteur & handle structs

--- fn.rs
+++ fn.rs
@@ -1,28 +1,20 @@
 fn main() {
-    // let mut age = 13;
-    // println!("{}", age);
-    // age = 32;
-    // println!("{}", age);
+    let mut age = 13;
+    println!("{}", age);
+    age = 32;
+    println!("{}", age);
 
-    // # vecteur 
-    // let date = vec!(2021, 2022);
-    // print(&date);
-    // print(&date);
-
-
-    // 3
-
+    // # vecteur
+    let date = vec![2021, 2022];
+    print(&date);
+    print(&date);
 }
 
 //  Vecteur
-// fn print(dn: &Vec<i32>) {
-//     println!("date is {:?}", dn);
-// }
-
+fn print(dn: &Vec<i32>) {
+    println!("date is {:?}", dn);
+}
 
 // regrouper des champs
-    // structure = pas de comportement
-    // class = avec comportent
-struct Foo {
-    value: i32
-}
+// structure = no comportement
+// class =  comportent
