56a54faf4b839c9082332f9966b47934058b5630
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-13 22:19:03 +0100

    upgrade cargo

---
+++ cargo/target/debug/cargo.d
@@ -0,0 +1,1 @@
+/home/root3/root/projet/RUST/cargo/target/debug/cargo: /home/root3/root/projet/RUST/cargo/src/main.rs
---
+++ md
@@ -0,0 +1,7 @@
+CARGO INSTALL Trunk
+rustup add wasm32-unknown-unknown
+
+
+cargo new webapp
+
+Cle LEDJER
