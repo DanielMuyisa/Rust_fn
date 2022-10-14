68fa8bcb15bc9236e1d4cd7491257f39c2101172
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-14 05:40:50 +0100

    update

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -8,5 +8,14 @@ fn main() {
         .read_line(&mut nbr)
         .expect("Fsil to read the number");
 
-    println! {"The number you've tap is {nbr}"}
+    println!("Saisir le deuxieme nbr");
+    let mut nbr2 = String::new();
+
+    io::stdin()
+        .read_line(&mut nbr2)
+        .expect("Fsil to read the number");
+
+    let sum = nbr + nbr2;
+
+    println! {"The number you've tap is {sum}"}
 }
