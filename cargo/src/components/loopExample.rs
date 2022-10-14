eacd6bf139c5ad142b0393a9f160a956ac0ef150
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-14 13:16:28 +0100

    data types

--- cargo/src/components/loopExample.rs
+++ cargo/src/components/loopExample.rs
@@ -4,25 +4,33 @@ use std::io;
 
 fn main() {
     println!("Enter a number");
+    let random = rand::thread_rng().gen_range(1..=100);
 
     loop {
         let mut input = String::new();
-        let random = rand::thread_rng().gen_range(1..=100);
 
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
 
+        // convert
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("You've enter {input}");
         println!("the random gen is {}", random);
 
         // comparison
-        let input: u32 = input.trim().parse().expect("Please type a number!");
 
         match input.cmp(&random) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         };
 
         println!("=========================");
--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -1,31 +1,65 @@
-use rand::Rng;
-use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Enter a number");
+    let float = 4.5;
+    let float: f32 = 40.1;
+    // println!("{float}");
 
-    loop {
-        let mut input = String::new();
-        let random = rand::thread_rng().gen_range(1..=100);
+    operations();
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
+    // Bool with explicit type
+    let bl = true;
+    let bl: bool = false;
+    // println!("{bl}");
 
-        println!("You've enter {input}");
-        println!("the random gen is {}", random);
+    // ------ TUPLE
+    let tuple: (i32, f64, u8) = (400, 5.3, 1);
+    let (x, y, z) = tuple;
 
-        // comparison
-        let input: u32 = input.trim().parse().expect("Please type a number!");
+    // println!("the value of z is {z}");
+    // println!("{}", { tuple.0 });
 
-        match input.cmp(&random) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
-        };
+    // ------TABLE
+    let table = [1, 2, 3, 4, 5];
+    // presise length
+    let tab: [i32; 3] = [1, 2, 3];
+    let months = [
+        "Janvier", "Fevrier", "Mars", "Avril", " Mais", "June", "July",
+    ];
+    println!("{}", months[0]);
 
-        println!("=========================");
-        println!("Enter a other number");
+    println!("======== ENTER A NUMBER =========");
+
+    // saisir
+    let mut input = String::new();
+    // save value
+    io::stdin().read_line(&mut input).expect("Valeur invalide");
+    // convert
+
+    let userValue: usize = input.trim().parse().expect("Value must be intege");
+    if (userValue > 0 && userValue < 8) {
+        println!("Conresponded value is {}", months[userValue - 1]);
+    } else {
+        println!("Non prise en charge");
     }
+}
+
+fn operations() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // println!("{remainder}, {quotient} ");
 }
